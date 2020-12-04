7459528271094781fc993ec3dc71b987341834a1
JacoV <jvosloo@imqs.co.za>
2020-12-04 20:07:26 +0200

    Revert "Adds support to forego waiting for both network connections and postgres connections"
    This reverts commit e45b1064

--- wait-for-nc.sh
+++ wait-for-nc.sh
@@ -22,7 +22,7 @@ USAGE
 wait_for() {
   for i in `seq $TIMEOUT` ; do
     nc -z "$HOST" "$PORT" > /dev/null 2>&1
-
+    
     result=$?
     if [ $result -eq 0 ] ; then
       if [ $# -gt 0 ] ; then
@@ -36,51 +36,44 @@ wait_for() {
   exit 1
 }
 
-if [ -z "WAIT_FOR_NC" ]; then
-  if [ "$WAIT_FOR_POSTGRES" == "true" ] ||
-     [ "$WAIT_FOR_POSTGRES" == "True" ] ||
-	 [ "$WAIT_FOR_POSTGRES" == "TRUE" ] ||
-	 [ "$WAIT_FOR_POSTGRES" == "1" ];then
-    while [ $# -gt 0 ]
-    do
-      case "$1" in
-        *:* )
-        HOST=$(printf "%s\n" "$1"| cut -d : -f 1)
-        PORT=$(printf "%s\n" "$1"| cut -d : -f 2)
-        shift 1
-        ;;
-        -q | --quiet)
-        QUIET=1
-        shift 1
-        ;;
-        -t)
-        TIMEOUT="$2"
-        if [ "$TIMEOUT" = "" ]; then break; fi
-        shift 2
-        ;;
-        --timeout=*)
-        TIMEOUT="${1#*=}"
-        shift 1
-        ;;
-        --)
-        shift
-        break
-        ;;
-        --help)
-        usage 0
-        ;;
-        *)
-        echoerr "Unknown argument: $1"
-        usage 1
-        ;;
-      esac
-    done
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    *:* )
+    HOST=$(printf "%s\n" "$1"| cut -d : -f 1)
+    PORT=$(printf "%s\n" "$1"| cut -d : -f 2)
+    shift 1
+    ;;
+    -q | --quiet)
+    QUIET=1
+    shift 1
+    ;;
+    -t)
+    TIMEOUT="$2"
+    if [ "$TIMEOUT" = "" ]; then break; fi
+    shift 2
+    ;;
+    --timeout=*)
+    TIMEOUT="${1#*=}"
+    shift 1
+    ;;
+    --)
+    shift
+    break
+    ;;
+    --help)
+    usage 0
+    ;;
+    *)
+    echoerr "Unknown argument: $1"
+    usage 1
+    ;;
+  esac
+done
 
-    if [ "$HOST" = "" -o "$PORT" = "" ]; then
-      echoerr "Error: you need to provide a host and port to test."
-      usage 2
-    fi
-
-    wait_for "$@"
-  fi
+if [ "$HOST" = "" -o "$PORT" = "" ]; then
+  echoerr "Error: you need to provide a host and port to test."
+  usage 2
 fi
+
+wait_for "$@"
