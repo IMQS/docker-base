9aad36fa7187967b119032319f9d6f1750e8a9a3
FritzOnFire <fritz.engl@gmail.com>
2025-11-07 14:48:58 +0200

    feat(postgres): Adds version 17 images
    Refs: ASG-4602

---
+++ postgres/qa-17/initdb/01-postgres-config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "wal_level = minimal" >> "$PGDATA/postgresql.conf"
+echo "max_wal_senders = 0" >> "$PGDATA/postgresql.conf"
+echo "fsync = off" >> "$PGDATA/postgresql.conf"
+echo "synchronous_commit = off" >> "$PGDATA/postgresql.conf"
+echo "full_page_writes = off" >> "$PGDATA/postgresql.conf"
+
+if [[ -n "$MAX_CONNECTIONS" ]]; then
+	echo "max_connections = $MAX_CONNECTIONS" >> "$PGDATA/postgresql.conf"
+fi
